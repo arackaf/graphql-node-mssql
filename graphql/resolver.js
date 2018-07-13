e1ee89847d80f2125e3115c1442e1a22641f6700
Adam Rackis <arackis@centralreach.com>
2018-07-12 19:28:33 -0500

    object graph

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,10 +1,35 @@
 const sql = require("mssql");
 const connString = require("../connstring");
 const conn = sql.connect(connString);
-
-const SELECT = `SELECT TOP 20 description, isCompleted, dueDate, creationDate, completedDate, completedByContactId FROM BE2_Tasks t WHERE OrganizationId = 189272 and description != '' `;
+const SELECT = `SELECT TOP 20 id, description, isCompleted, dueDate, creationDate, completedDate, completedByContactId FROM BE2_Tasks t WHERE OrganizationId = 189272 and description != '' `;
 
 module.exports = {
+  Comment: {
+    async attachments(comment) {
+      let pool = await conn;
+      let result = await pool
+        .request()
+        .query(
+          `SELECT r.id, r.fileName, r.name FROM BE2_TaskCommentResources tcr JOIN BE_Resources r ON tcr.ResourceId = r.Id WHERE tcr.CommentId = ` +
+            comment.id
+        );
+      return result.recordsets[0];
+    }
+  },
+  Task: {
+    async comments(task) {
+      let pool = await conn;
+      let result = await pool.request().query(`SELECT id, description FROM BE2_TaskComments WHERE TaskId = ` + task.id);
+      return result.recordsets[0];
+    },
+    async attachments(task) {
+      let pool = await conn;
+      let result = await pool
+        .request()
+        .query(`SELECT r.id, r.fileName, r.name FROM BE2_TaskResources tr JOIN BE_Resources r ON tr.ResourceId = r.Id WHERE tr.TaskId = ` + task.id);
+      return result.recordsets[0];
+    }
+  },
   Query: {
     async allTasks(root, args, context, ast) {
       let pool = await conn;
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,12 +1,27 @@
 module.exports = `
 
+type Attachment { 
+  id: Int
+  fileName: String
+  name: String
+}
+
+type Comment { 
+  id: Int
+  description: String
+  attachments: [Attachment]
+}
+
 type Task {
+  id: Int
   description: String
   isCompleted: Int
   dueDate: String
   creationDate: String
   completedDate: String
   completedByContactId: Int
+  comments: [Comment]
+  attachments: [Attachment]
 }
 
 type Query {
